a9e204dc46a0106730199e724629db3ea1b1096a
dimanzver <diman.suvor@yandex.ru>
2023-09-01 07:25:53 +0300

    php 8 compatibility

--- tests/Actions/OrdersTest.php
+++ tests/Actions/OrdersTest.php
@@ -25,7 +25,7 @@ class OrdersTest extends TestCase
     protected $orders;
 
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $psr18Client = new Psr18Client();
@@ -33,12 +33,9 @@ class OrdersTest extends TestCase
         $client->setTest(true);
         $this->orders = $client->orders();
         \Doctrine\Common\Annotations\AnnotationReader::addGlobalIgnoredName('phan');
-
-        /** @phan-suppress-next-line PhanDeprecatedFunction */
-        \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
     }
 
-    protected function tearDown()
+    protected function tearDown(): void
     {
         parent::tearDown();
         $this->orders = null;
